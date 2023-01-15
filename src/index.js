f434f183fb9c70492c9099b4685dac9d9d4edc3c
TinhTran5555 <tranvotantinh@gmail.com>
2023-01-15 12:43:56 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-// const express = require('express');
-// const {sequelize} = require('./models')
-
-// const {handleErrors} = require('./helpers/error');
-// const configs = require('./config');
-// const server = express()
-// const app = express();
-// app.use(express.json());
-// app.use(express.static('.'));
-
-// sequelize.sync({ alter: true });
-
-// const v1 = require('./routers/v1');
-// app.use('/capstone/v1',v1);
-
-// app.use(handleErrors);
-
-// app.listen(configs.PORT);
 const express = require('express');
+const {sequelize} = require('./models')
+
+const {handleErrors} = require('./helpers/error');
+const configs = require('./config');
 const server = express()
-server.use((req,res)=> {
-    res.send("hello")
-})
-const PORT = process.env.PORT || 3000
-server.listen(PORT,()=>{
-    console.log("server connect tinhtran");
-})
+const app = express();
+app.use(express.json());
+app.use(express.static('.'));
+
+sequelize.sync({ alter: true });
+
+const v1 = require('./routers/v1');
+app.use('/capstone/v1',v1);
+
+app.use(handleErrors);
+
+app.listen(configs.PORT);
+// const express = require('express');
+// const server = express()
+// server.use((req,res)=> {
+//     res.send("hello")
+// })
+// const PORT = process.env.PORT || 3000
+// server.listen(PORT,()=>{
+//     console.log("server connect tinhtran");
+// })
